ee383319e1a4e4f7d40c11ae7df9054b000a08b2
Gj1337 <valentyn.hlyvich@gmail.com>
2024-11-15 12:49:51 +0200

    1574. Shortest Subarray to be Removed to Make Array Sorted

---
+++ 1574. Shortest Subarray to be Removed to Make Array Sorted.dart
@@ -0,0 +1,75 @@
+// Given an integer array arr, remove a subarray (can be empty) from arr such that the remaining elements in arr are non-decreasing.
+
+// Return the length of the shortest subarray to remove.
+
+// A subarray is a contiguous subsequence of the array.
+
+// Example 1:
+
+// Input: arr = [1,2,3,10,4,2,3,5]
+// Output: 3
+// Explanation: The shortest subarray we can remove is [10,4,2] of length 3. The remaining elements after that will be [1,2,3,3,5] which are sorted.
+// Another correct solution is to remove the subarray [3,10,4].
+// Example 2:
+
+// Input: arr = [5,4,3,2,1]
+// Output: 4
+// Explanation: Since the array is strictly decreasing, we can only keep a single element. Therefore we need to remove a subarray of length 4, either [5,4,3,2] or [4,3,2,1].
+// Example 3:
+
+// Input: arr = [1,2,3]
+// Output: 0
+// Explanation: The array is already non-decreasing. We do not need to remove any elements.
+
+// Constraints:
+
+// 1 <= arr.length <= 105
+// 0 <= arr[i] <= 109
+
+import 'tastable.dart';
+
+class Solution {
+  int findLengthOfShortestSubarray(List<int> arr) {
+    var right = arr.length - 1 > 0 ? arr.length - 1 : 0;
+
+    while ((right > 0) && (arr[right] >= arr[right - 1])) {
+      right--;
+    }
+
+    var ans = right;
+
+    var left = 0;
+    while ((left < right) && (left == 0 || arr[left - 1] <= arr[left])) {
+      while (right < arr.length && arr[left] > arr[right]) {
+        right++;
+      }
+
+      ans = min(ans, right - left - 1);
+
+      left++;
+    }
+
+    return ans;
+  }
+}
+
+int min(int a, int b) => a < b ? a : b;
+
+class CurrentTestCase extends Testable {
+  CurrentTestCase({required this.arr, super.result});
+
+  final List<int> arr;
+
+  @override
+  computeResult() => Solution().findLengthOfShortestSubarray(arr);
+}
+
+void main(List<String> args) {
+  [
+    CurrentTestCase(arr: [], result: 0),
+    CurrentTestCase(arr: [1, 2, 3, 10, 4, 2, 3, 5], result: 3),
+    CurrentTestCase(arr: [5, 4, 3, 2, 1], result: 4),
+    CurrentTestCase(arr: [1, 2, 3], result: 0),
+    CurrentTestCase(arr: [1, 2, 3, 10, 0, 7, 8, 9], result: 2)
+  ].test();
+}
--- tastable.dart
+++ tastable.dart
@@ -1,26 +1,5 @@
-// abstract class TestCase<T extends Object> {
-//   TestCase(this.result);
-
-//   final T result;
-// }
-
-// extension TestExstention<T extends TestCase> on List<T>{
-
-//   void test(T Function() testFunction){
-//       for (var i = 0; i < this.length; i++) {
-//     final testCase = this[i];
-
-//     final result = testFunction.call();
-//     final isCorrect = result == testCase.result;
-
-//     print('Test case $i ${isCorrect ? '✅' : '❌'}');
-//   }
-//   }
-
-// }
-
 abstract class Testable<T> {
-  Testable(this.result);
+  Testable({required this.result});
 
   final T result;
 
@@ -31,11 +10,14 @@ extension TestingListExtention<T extends Testable> on List<T> {
   void test() {
     for (var i = 0; i < this.length; i++) {
       final testCase = this[i];
-      final result = testCase.computeResult.call();
+      final result = testCase.computeResult();
 
       final isCorrect = result == testCase.result;
 
-      print('Test case $i ${isCorrect ? '✅' : '❌'}');
+      print(
+        'Test case $i '
+        '${isCorrect ? '✅' : '❌\r\nCorrect result $result!=${testCase.result}\r\n'}',
+      );
     }
   }
 }
