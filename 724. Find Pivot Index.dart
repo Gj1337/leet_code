6524aee017cc23506974638c180bf170d88fe246
Gj1337 <valentyn.hlyvich@gmail.com>
2025-08-06 11:27:44 +0300

    724. Find Pivot Index

---
+++ 724. Find Pivot Index.dart
@@ -0,0 +1,76 @@
+// Given an array of integers nums, calculate the pivot index of this array.
+// The pivot index is the index where the sum of all the numbers
+// strictly to the left of the index is equal to the sum of all the numbers
+// strictly to the index's right.
+// If the index is on the left edge of the array, then the left sum is 0
+// because there are no elements to the left.
+// This also applies to the right edge of the array.
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+// Example 1:
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+// Example 2:
+// Input: nums = [1,2,3]
+// Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
+
+// Example 3:
+// Input: nums = [2,1,-1]
+// Output: 0
+// Explanation:
+// The pivot index is 0.
+// Left sum = 0 (no elements to the left of index 0)
+// Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+// Constraints:
+//     1 <= nums.length <= 104
+//     -1000 <= nums[i] <= 1000
+
+import 'testable.dart';
+
+class Solution {
+  int pivotIndex(List<int> nums) {
+    if (nums.isEmpty) return -1;
+    if (nums.length == 1) return 0;
+
+    final rightPrefixSum = List.filled(nums.length, nums.last);
+    for (var i = nums.length - 2; i >= 0; i--) {
+      rightPrefixSum[i] = rightPrefixSum[i + 1] + nums[i];
+    }
+
+    var leftSum = 0;
+    for (var i = 0; i < nums.length; i++) {
+      leftSum += nums[i];
+
+      if (rightPrefixSum[i] - leftSum == 0) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
+
+class SolutionTest extends Testable<int> with ConsoleTestOutput {
+  SolutionTest({required this.nums, required super.result});
+
+  final List<int> nums;
+
+  @override
+  int computeResult() => Solution().pivotIndex(nums);
+}
+
+void main(List<String> args) {
+  [
+    SolutionTest(nums: [1, 7, 3, 6, 5, 6], result: 3),
+    SolutionTest(nums: [1, 2, 3], result: -1),
+    SolutionTest(nums: [2, 1, -1], result: 0),
+  ].test();
+}
