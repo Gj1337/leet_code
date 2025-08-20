89b4d04ba5ae317e6c65ccc5b2f8120e3bc627df
Gj1337 <valentyn.hlyvich@gmail.com>
2025-08-21 00:03:26 +0300

    2095. Delete the Middle Node of a Linked List

---
+++ 2095. Delete the Middle Node of a Linked List.dart
@@ -0,0 +1,108 @@
+//   You are given the head of a linked list. Delete the middle node,
+// and return the head of the modified linked list.
+// The middle node of a linked list of size n
+// is the ⌊n / 2⌋th node from the start using 0-based indexing,
+// where ⌊x⌋ denotes the largest integer less than or equal to x.
+//     For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2,
+// respectively.
+
+// Example 1:
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list.
+// The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node.
+
+// Example 2:
+// Input: head = [1,2,3,4]
+// Output: [1,2,4]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+
+// Example 3:
+// Input: head = [2,1]
+// Output: [2]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+// Node 0 with value 2 is the only node remaining after removing node 1.
+
+// Constraints:
+//     The number of nodes in the list is in the range [1, 105].
+//     1 <= Node.val <= 105
+
+// Definition for singly-linked list.
+import 'testable.dart';
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+extension ListNodeExtension on ListNode? {
+  List<int> toList() {
+    final result = <int>[];
+    ListNode? current = this;
+    while (current != null) {
+      result.add(current.val);
+      current = current.next;
+    }
+    return result;
+  }
+
+  static ListNode? fromList(List<int> list) {
+    if (list.isEmpty) return null;
+
+    final head = ListNode(list.first);
+    ListNode? current = head;
+    for (int i = 1; i < list.length; i++) {
+      current?.next = ListNode(list[i]);
+      current = current?.next;
+    }
+    return head;
+  }
+}
+
+class Solution {
+  ListNode? deleteMiddle(ListNode? head) {
+    if (head?.next == null) return null;
+
+    var slowPointer = head;
+    var fastPointer = head?.next?.next;
+
+    while (fastPointer?.next != null) {
+      slowPointer = slowPointer?.next;
+      fastPointer = fastPointer?.next?.next;
+    }
+
+    slowPointer?.next = slowPointer.next?.next;
+
+    return head;
+  }
+}
+
+class SolutionTest extends Testable<List<int>> with ConsoleTestOutput {
+  final List<int> linkedList;
+
+  SolutionTest({required this.linkedList, required super.result});
+
+  @override
+  List<int> computeResult() {
+    final head = ListNodeExtension.fromList(linkedList);
+    final reversedHead = Solution().deleteMiddle(head);
+
+    return reversedHead?.toList() ?? [];
+  }
+}
+
+void main(List<String> args) {
+  [
+    SolutionTest(linkedList: [1, 3, 4, 7, 1, 2, 6], result: [1, 3, 4, 1, 2, 6]),
+    SolutionTest(linkedList: [1, 2, 3, 4], result: [1, 2, 4]),
+    SolutionTest(linkedList: [2, 1], result: [2]),
+  ].test();
+}
