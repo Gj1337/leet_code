b6eb3d488a80654fdea988da43d77a4a92140f1e
Gj1337 <valentyn.hlyvich@gmail.com>
2025-10-03 02:52:33 +0300

    303. Range Sum Query - Immutable

---
+++ 303. Range Sum Query - Immutable.dart
@@ -0,0 +1,42 @@
+// Given an integer array nums, handle multiple queries of the following type:
+//     Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+//     NumArray(int[] nums) Initializes the object with the integer array nums.
+//     int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+// Example 1:
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+// Constraints:
+//
+//     1 <= nums.length <= 104
+//     -105 <= nums[i] <= 105
+//     0 <= left <= right < nums.length
+//     At most 104 calls will be made to sumRange.
+// [1,2,3,4,5]
+// [1,3,6,10,15]
+// [15,14,12,9,5]
+// 1-3 = 9
+// 2
+
+class NumArray {
+  late final _prefixSum;
+
+  NumArray(List<int> nums) {
+    var sum = 0;
+    _prefixSum = nums.map((num) => sum + num).toList();
+  }
+
+  int sumRange(int left, int right) =>
+      _prefixSum[right] - (left != 0 ? _prefixSum[left - 1] : 0);
+}
